fe93150d2863051f79ef9e865b68219e6d7ea277
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-05-17 23:02:06 +0800

    style: format files with Prettier

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <button onClick={() => setCount((count) => count + 1)}>
       count is {count}
     </button>
-  )
+  );
 }
