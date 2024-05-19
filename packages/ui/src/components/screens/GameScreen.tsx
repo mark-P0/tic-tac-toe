4c2c839925ecf32468c3b682eea987bbf3382997
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-05-19 14:49:18 +0800

    refactor(ui): move session functions to context file

--- packages/ui/src/components/screens/GameScreen.tsx
+++ packages/ui/src/components/screens/GameScreen.tsx
@@ -1,103 +1,12 @@
 import clsx from "clsx";
 import { ComponentProps, useState } from "react";
-import { raise } from "utils/errors";
 import {
   Cell,
   Player,
-  Round,
+  getRoundInfo,
   useSessionContext,
+  useSessionInfo,
 } from "../../contexts/SessionContext";
-
-function getWinningIndices(board: Round["board"]) {
-  const slices = [
-    // Horizontals
-    ...[
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-    ],
-    // Verticals
-    ...[
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 8, 5],
-    ],
-    // Diagonals
-    ...[
-      [0, 4, 8],
-      [2, 4, 6],
-    ],
-  ];
-
-  for (const slice of slices) {
-    const [a, b, c] = slice;
-
-    if (
-      board[a] !== null &&
-      board[a] === board[b] &&
-      board[a] === board[c] &&
-      board[b] === board[c]
-    )
-      return slice;
-  }
-
-  return null;
-}
-
-function getRoundInfo(round: Round) {
-  const winningIndices = getWinningIndices(round.board);
-
-  const hasWinner = winningIndices !== null;
-  const hasFreeBoardCells = round.board.includes(null);
-  const isRoundOver = hasWinner || !hasFreeBoardCells;
-
-  type Winner =
-    | Player // "Symbol" of a player
-    | null // Draw (no one won)
-    | undefined; // No winner yet; round is possibly on-going or left unfinished
-  let winner: Winner = null;
-  if (!isRoundOver) {
-    winner = undefined;
-  }
-  if (hasWinner) {
-    const idx = winningIndices[0];
-    winner = round.board[idx];
-  }
-
-  return {
-    ...{ winningIndices, winner },
-    ...{ hasWinner, hasFreeBoardCells, isRoundOver },
-  };
-}
-
-function useSessionInfo() {
-  const { session } = useSessionContext();
-  const { players, rounds } = session;
-
-  if (players === null) {
-    raise("Players does not exist...?");
-  }
-  players satisfies NonNullable<typeof players>;
-
-  let player1Wins = 0;
-  let player2Wins = 0;
-  let draws = 0;
-  for (const round of rounds) {
-    const { winner } = getRoundInfo(round);
-    if (winner === "x") player1Wins++;
-    if (winner === "o") player2Wins++;
-    if (winner === null) draws++;
-  }
-
-  const round =
-    rounds[rounds.length - 1] ?? raise("Current round does not exist...?");
-
-  return {
-    ...{ players, rounds },
-    ...{ player1Wins, player2Wins, draws },
-    ...{ round },
-  };
-}
 
 function BoardCellButton(props: {
   cell: Cell;
