6d8cefa9200a02bc3cc45ed855c23a5e16833ec4
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-05-19 10:33:05 +0800

    feat(ui): initialize game screen layout

--- packages/ui/src/components/screens/GameScreen.tsx
+++ packages/ui/src/components/screens/GameScreen.tsx
@@ -1,15 +1,75 @@
-import { useScreenContext } from "../../contexts/ScreenContext";
+import { raise } from "utils/errors";
 import { useSessionContext } from "../../contexts/SessionContext";
 
 export function GameScreen() {
-  const { changeScreen } = useScreenContext();
   const { session } = useSessionContext();
 
-  return (
-    <article>
-      <button onClick={() => changeScreen("home")}>Go to home screen</button>
+  const { players, rounds } = session;
+  if (players === null) {
+    raise("Players does not exist...?");
+  }
+  players satisfies NonNullable<typeof players>;
 
-      <pre>{JSON.stringify(session, undefined, 2)}</pre>
+  let player1Wins = 0;
+  let player2Wins = 0;
+  let draws = 0;
+  for (const round of rounds) {
+    if (round.winner === "x") player1Wins++;
+    if (round.winner === "o") player2Wins++;
+    if (round.winner === null) draws++;
+  }
+
+  const round =
+    rounds[rounds.length - 1] ?? raise("Current round does not exist...?");
+
+  // TODO Add state for current player turn
+
+  // TODO Add logic for button click; should update round board, cycle to next player
+
+  // TODO Check if round board already has a winner
+  // TODO if with winner, disable form fieldset below
+  // TODO if with winner, useEffect update round, show game end modal
+
+  return (
+    <article className="bg-stone-300 h-screen w-screen grid grid-rows-[auto_1fr]">
+      <header className="grid grid-cols-3 p-6">
+        <div>
+          <h2 className="text-xl font-bold">{players[0]}</h2>
+          <ol className="flex gap-3">
+            <li className="font-bold">W {player1Wins}</li>
+            <li>L {player2Wins}</li>
+            <li className="text-black/50">D {draws}</li>
+          </ol>
+        </div>
+
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Round {rounds.length}</h1>
+        </div>
+
+        <div className="text-right">
+          <h2 className="text-xl font-bold">{players[1]}</h2>
+          <ol className="flex flex-row-reverse gap-3">
+            <li className="font-bold">W {player2Wins}</li>
+            <li>L {player1Wins}</li>
+            <li className="text-black/50">D {draws}</li>
+          </ol>
+        </div>
+      </header>
+
+      <form className="grid place-items-center">
+        <fieldset className="grid grid-cols-3 grid-rows-3 gap-6 p-6 rounded-lg aspect-square h-2/3 bg-stone-400">
+          {round.board.map((cell, idx) => (
+            <button
+              key={idx}
+              disabled={cell !== null}
+              className="bg-white rounded-lg"
+            >
+              {cell === "x" && <span className="text-6xl font-bold">X</span>}
+              {cell === "o" && <span className="text-6xl font-bold">O</span>}
+            </button>
+          ))}
+        </fieldset>
+      </form>
     </article>
   );
 }
