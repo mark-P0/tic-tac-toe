e07ef06cda9dcb7e36350a58bdc48d1a16331bb4
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-05-20 15:57:46 +0800

    feat(api): create endpoint for saving a new session

--- packages/api/src/routes/sessions.ts
+++ packages/api/src/routes/sessions.ts
@@ -1,7 +1,10 @@
 import { zSession } from "@tic-tac-toe/schemas";
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
-import { getAllSessionsFromMostToLeastRecent } from "../db/sessions";
+import {
+  getAllSessionsFromMostToLeastRecent,
+  saveNewSession,
+} from "../db/sessions";
 
 export const SessionsRouter = Router();
 
@@ -11,6 +14,25 @@ SessionsRouter.get("/sessions", async (req, res) => {
 
     const sessions = zSession.array().parse(dbSessions);
     res.json(sessions);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+});
+
+SessionsRouter.post("/sessions", async (req, res) => {
+  try {
+    const parsing = zSession.safeParse(req.body);
+    if (!parsing.success) {
+      console.error(parsing.error);
+      res.sendStatus(StatusCodes.BAD_REQUEST);
+      return;
+    }
+    const session = parsing.data;
+
+    await saveNewSession(session);
+
+    res.json(true);
   } catch (error) {
     console.error(error);
     res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
