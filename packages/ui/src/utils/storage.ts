fe81c6d6963b1397589fc81d7ef1ecb21770f98c
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-05-20 19:18:42 +0800

    feat(ui): parse local storage with zod

--- packages/ui/src/utils/storage.ts
+++ packages/ui/src/utils/storage.ts
@@ -1,9 +1,20 @@
-import { Session } from "@tic-tac-toe/schemas";
+import { Session, zSession } from "@tic-tac-toe/schemas";
 
-export function getSessionsFromStorage() {
-  // TODO Parse with Zod?
-  const sessionsFromStorage = localStorage.getItem("sessions") ?? "[]";
-  const sessions: Session[] = JSON.parse(sessionsFromStorage);
+export function getSessionsFromStorage(): Session[] {
+  const sessionsAsStr = localStorage.getItem("sessions");
+  if (sessionsAsStr === null) return [];
+
+  const sessionsAsObj = JSON.parse(sessionsAsStr);
+  const parsing = zSession.array().safeParse(sessionsAsObj);
+  if (!parsing.success) {
+    console.warn(parsing.error);
+    console.warn(
+      "Failed to parse stored sessions; defaulting to empty array..."
+    );
+    return [];
+  }
+
+  const sessions = parsing.data;
 
   return sessions;
 }
